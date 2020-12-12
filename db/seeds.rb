6bbcd231aab56da4b4ef2a6a02eec463f4f44bc7
Joao Marcos <jao.markos@hotmail.com>
2020-12-11 22:26:40 -0300

    Add seed for default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  name: 'João Marcos',
+  email: ENV['DEFAULT_ADMIN_EMAIL'],
+  password: ENV['DEFAULT_ADMIN_PASSWORD'],
+  admin: true
+)
