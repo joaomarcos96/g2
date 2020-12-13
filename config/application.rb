851e9f7b83261eea170e970ab9bf1319ef6e7599
Joao Marcos <jao.markos@hotmail.com>
2020-12-13 12:17:09 -0300

    Add pt-BR locale to devise

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module G2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = 'pt-BR'
   end
 end
