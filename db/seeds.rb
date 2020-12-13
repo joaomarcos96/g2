6dee9b7307df463b9910505b6774ba9e3b0c0eb4
Joao Marcos <jao.markos@hotmail.com>
2020-12-13 20:41:18 -0300

    Add news seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,15 @@ User.create(
   password: ENV['DEFAULT_ADMIN_PASSWORD'],
   admin: true
 )
+
+News.create(
+  title: 'Brasil tem média móvel de casos de Covid acima de 40 mil pelo 11º dia',
+  subtitle: 'País tem 181.419 óbitos e 6.901.990 diagnósticos de Covid-19 registrados desde o começo da pandemia, segundo levantamento do consórcio de imprensa.',
+  content: 'O consórcio de veículos de imprensa divulgou novo levantamento da situação da pandemia de coronavírus no Brasil a partir de dados das secretarias estaduais de Saúde, consolidados às 20h deste domingo (13).
+
+  O país registrou 276 mortes pela Covid-19 nas últimas 24 horas, chegando ao total de 181.419 óbitos desde o começo da pandemia. Com isso, a média móvel de mortes no Brasil nos últimos 7 dias foi de 637. A variação foi de +23% em comparação à média de 14 dias atrás, indicando tendência de alta nos óbitos pela doença.
+
+  Em casos confirmados, desde o começo da pandemia 6.901.990 brasileiros já tiveram ou têm o novo coronavírus, com 21.395 desses confirmados no último dia. A média móvel nos últimos 7 dias foi de 42.721 novos diagnósticos por dia. Isso representa uma variação de +20% em relação aos casos registrados em duas semanas, o que indica tendência de alta também nos diagnósticos.
+
+  É o 11º dia que essa média aparece acima da casa dos 40 mil. No primeiro pico da pandemia, entre o final de julho e o meio de agosto, o país chegou a registrar 27 dias consecutivos com a média móvel de diagnósticos acima de 40 mil. A maior marca registrada ocorreu em 27 de julho, com média de 46.393.'
+)
