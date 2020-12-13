28d54236b237889619e97da9a38ccc46ab633912
Joao Marcos <jao.markos@hotmail.com>
2020-12-13 13:16:26 -0300

    Include validations in news model

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,7 @@
 class News < ApplicationRecord
   has_many :comments, dependent: :destroy
 
-  validates :title, :content, presence: true
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :subtitle, presence: true, length: { maximum: 150 }
+  validates :content, presence: true
 end
